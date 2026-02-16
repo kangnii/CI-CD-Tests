c375232523ccbf565864723332712f42cd6788ba
kangnii <wisdomfollygan@gmail.com>
2026-02-16 09:33:00 +0100

    ajout xss

--- src/routes.js
+++ src/routes.js
@@ -9,4 +9,10 @@ router.get('/time', (req, res) => {
     res.json({ time: new Date().toISOString() });
 });
 
+router.get('/welcome', (req, res) => {
+const name = req.query.name || 'Visiteur'
+// VULNÉRABILITÉ : On renvoie l'input utilisateur sans filtrage
+res.send(`<h1>Bienvenue, ${name}</h1>`)
+});
+
 module.exports = router;
